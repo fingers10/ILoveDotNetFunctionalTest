26d49cb39bbc2995825caaa6ed18db0c4774e129
Abdul Rahman <abdulrahman.smsi@gmail.com>
2023-07-16 21:16:58 +0530

    Add project files.

---
+++ FunctionalTest/CustomWebApplicationFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace FunctionalTest;
+public class CustomWebApplicationFactory
+{
+    private static WebApplicationFactory<Program> Application => new WebApplicationFactory<Program>();
+
+    protected static async Task RunTest(Func<HttpClient, Task> test)
+    {
+        var client = Application.CreateClient(new WebApplicationFactoryClientOptions
+        {
+            AllowAutoRedirect = false
+        });
+        await test(client);
+    }
+}
---
+++ FunctionalTest/WeatherForecastControllerShouldTests.cs
@@ -0,0 +1,25 @@
+﻿using API;
+using FluentAssertions;
+using System.Net;
+using System.Text.Json;
+
+namespace FunctionalTest;
+public class WeatherForecastControllerShouldTests : CustomWebApplicationFactory
+{
+    [Fact]
+    public Task ReturnExpectedResponse()
+    {
+        return RunTest(async (client) =>
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Get, $"/weatherforecast");
+            var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+
+            var responseStream = await response.Content.ReadAsStreamAsync();
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            var result = await JsonSerializer.DeserializeAsync<List<WeatherForecast>>(responseStream, options);
+            
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            result!.Count.Should().Be(5);
+        });
+    }
+}
